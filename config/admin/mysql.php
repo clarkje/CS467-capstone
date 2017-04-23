22a750ea3a21f937e6cf0a89e961ea277e185532
clarkje <clarkje@oregonstate.edu>
2017-04-22 17:56:58 -0700

    Updated the MySQL config file
    This works for local development.  We’ll need to manage the production
    config file out of band, and ensure it doesn’t get overwritten when
    syncing to the server.

--- config/admin/mysql.php
+++ config/admin/mysql.php
@@ -1,6 +1,21 @@
 <?php
-$db_host = "your_db_host";
-$db_user = "your_user";
-$db_pass = "your_pass";
-$db_schema = "your_db";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "root";
+$db_schema = "phoenixdb";
+
+$mysqli = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+<!-- TODO: Turn error reporting off in production -->
+if($mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
 ?>
