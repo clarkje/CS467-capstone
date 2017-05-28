dc905833c622ecb3760ce75a68fa5d6af1eb135e
clarkje <clarkje@oregonstate.edu>
2017-05-28 15:50:22 -0700

    Add Report Navigation, Report By Region
    Generates Awards By Region report on arbitrary dates.

--- db/AwardManager.php
+++ db/AwardManager.php
@@ -33,6 +33,68 @@ class AwardManager {
 
 
   /**
+  * Returns a count of awards by the user that granted them
+  * @param DateTime $startDate
+  * @param DateTime $endDate
+  * @return Array array of regions and counts
+  */
+
+  public function getAwardCountByGranter($startDate = null, $endDate = null, $limit = null) {
+
+
+  }
+
+  /**
+  * Returns a count of awards granted by region for a specified date window
+  * @param DateTime $startDate
+  * @param DateTime $endDate
+  * @return Array array of regions and counts
+  */
+
+  public function getAwardCountByRegion($startDate = null, $endDate = null) {
+
+    $queryString = "SELECT count(a) AS awardCount, r.name
+                    FROM Award a
+                    JOIN a.recipientAddress addr
+                    JOIN addr.country c
+                    JOIN c.region r";
+
+    if(!empty($startDate) || !empty($endDate)) {
+      $queryString .= " WHERE ";
+    }
+
+    if(!empty($startDate)) {
+      $queryString .= " a.grantDate > :startDate";
+    }
+
+    if(!empty($startDate) && !empty($endDate)) {
+      $queryString .= " AND ";
+    }
+
+    if(!empty($endDate)) {
+      $queryString .= " a.grantDate < :endDate";
+    }
+
+    $queryString .= " GROUP BY r.name";
+
+    $query = $this->em->createQuery($queryString);
+
+    $params = array();
+    if(!empty($startDate)) {
+      $query->setParameter('startDate', $startDate);
+    }
+
+    if(!empty($endDate)) {
+      $query->setParameter('endDate', $endDate);
+    }
+
+    $result = $query->getResult();
+    return $result;
+  }
+
+
+
+  /**
   * @return Array of Awards
   */
   public function loadAll($orderBy = null, $limit = null, $offset = null) {
