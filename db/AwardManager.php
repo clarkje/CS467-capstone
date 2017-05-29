9fb30666614e86daa70dd61855045b55b3d56367
clarkje <clarkje@oregonstate.edu>
2017-05-28 17:35:20 -0700

    Report - Awards by Receipient

--- db/AwardManager.php
+++ db/AwardManager.php
@@ -38,6 +38,52 @@ class AwardManager {
   * @param DateTime $endDate
   * @return Array array of regions and counts
   */
+  public function getAwardCountByRecipient($startDate = null, $endDate = null, $limit = 25) {
+
+    $queryString = "SELECT count(a.id) AS awardCount, a.recipientFirst AS firstName, a.recipientLast AS lastName
+                      FROM Award a
+    ";
+
+    if(!empty($startDate) || !empty($endDate)) {
+      $queryString .= " WHERE ";
+    }
+
+    if(!empty($startDate)) {
+      $queryString .= " a.grantDate > :startDate";
+    }
+
+    if(!empty($startDate) && !empty($endDate)) {
+      $queryString .= " AND ";
+    }
+
+    if(!empty($endDate)) {
+      $queryString .= " a.grantDate < :endDate";
+    }
+
+    $queryString .= " GROUP BY a.recipientFirst, a.recipientLast ORDER BY awardCount DESC";
+
+    $query = $this->em->createQuery($queryString);
+
+    $query->setMaxResults($limit);
+
+    if(!empty($startDate)) {
+      $query->setParameter('startDate', $startDate);
+    }
+
+    if(!empty($endDate)) {
+      $query->setParameter('endDate', $endDate);
+    }
+
+    $result = $query->getResult();
+    return $result;
+  }
+
+  /**
+  * Returns a count of awards by the user that granted them
+  * @param DateTime $startDate
+  * @param DateTime $endDate
+  * @return Array array of regions and counts
+  */
   public function getAwardCountByGranter($startDate = null, $endDate = null, $limit = null) {
 
     $queryString = "SELECT count(a) AS awardCount, u.firstName, u.lastName
