ebee2d988db32bfe32026e51211c4041c81d08c5
clarkje <clarkje@oregonstate.edu>
2017-06-07 08:33:27 -0700

    Bugfix: Awards by Date report Failing
    This got mangled in a merge, and somehow I checked in the production
    credentials…

--- config/admin/doctrine.php
+++ config/admin/doctrine.php
@@ -4,8 +4,35 @@
 // http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/configuration.html
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
 
 require_once(__DIR__ . "/../config.php");
+
+// Borrowed From:
+// https://stackoverflow.com/questions/33899120/symfony-doctrine-get-entries-count-by-day-on-a-datetime-field
+
+/**
+* Extracts the date form a timestamp (Mysql function)
+* DQLDate :: DATE ( date as Y-m-d )
+*/
+class DQLDate extends FunctionNode
+{
+  protected $date;
+  public function getSql(SqlWalker $sqlWalker) {
+    return 'DATE(' . $sqlWalker->walkArithmeticPrimary($this->date) .')';
+  }
+
+  public function parse(Parser $parser)
+  {
+    $parser->match(Lexer::T_IDENTIFIER);
+    $parser->match(Lexer::T_OPEN_PARENTHESIS);
+    $this->date = $parser->ArithmeticPrimary();
+    $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+   }
+}
 
 class EntityManagerFactory
 {
@@ -19,9 +46,12 @@ class EntityManagerFactory
 
     // Database configuration pg_parameter_status
     $connectionParams = array(
-      'url' => 'mysql://phoenixadmin:j94Qm$CZ^%$J@phoenixdb.jeromie.com/jeromiecom_phoenix');    
+    'url' => 'mysql://root:root@127.0.0.1:8889/phoenix_admin'
+    );
 
     $config = Setup::createAnnotationMetadataConfiguration(array($GLOBALS['DOCUMENT_ROOT'] . "/db/src"), $isDevMode);
+    $config->addCustomDateTimeFunction('DATE', 'DQLDate');
+    
     $this->entityManager = EntityManager::create($connectionParams, $config);
   }
 
