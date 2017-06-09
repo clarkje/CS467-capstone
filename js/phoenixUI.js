05b7a8e50b4c21502ef8676c56d67547d2ee6522
clarkje <clarkje@oregonstate.edu>
2017-06-08 17:29:50 -0700

    Bugfix: Fixed some JavaScript errors
    This script runs on every page, but it really needs to only run on one
    or two.  I put some checks in so that it won’t fail when run on other
    pages, but we should move the function calls into <script> tags on the
    appropriate pages.

--- js/phoenixUI.js
+++ js/phoenixUI.js
@@ -2,19 +2,25 @@
 function removeSpinner(){
   //remove the spinner after animation completes
   var spinner = document.getElementById("spinner-wrapper");
-  spinner.remove(0);
+  if (spinner) {
+    spinner.remove(0);
+  }
 
   //change pdf position relative to ensure page layout does not chagne
   var pdf = document.getElementById(certificate);
-  certificate.style.position = "relative";
+  if (pdf) {
+    certificate.style.position = "relative";
+  }
 }
 
 function showPDF(){
   //change pdf display relative to ensure page layout does not chagne
   var pdf = document.getElementById("certificate");
-  pdf.style.display = "inline-block";
+  if(pdf) {
+    pdf.style.display = "inline-block";
+  }
 }
 
+// These calls should really be moved to the page(s) they apply to
 myVar = setTimeout(removeSpinner, 6500);
-
 pdf = setTimeout(showPDF, 6000);
